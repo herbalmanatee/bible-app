feef3792d746440536587cd054c1bdefd8735415
William <williameliason8@gmail.com>
2021-01-09 00:57:41 -0600

    connected to local db and heroku db (postgres)

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const db = require('../db');
 const app = express();
 const port = process.env.PORT || 8080;
-
+// console.log(process.env.;
 app.use(express.static('dist'));
 app.listen(port, ()=> {
   console.log(`listening on ${port}`);
