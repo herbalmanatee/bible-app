d9b1285b8528e05ca6b52f6347ed38d720813dc1
William <williameliason8@gmail.com>
2021-01-09 23:55:51 -0600

    add version form and its submit handler

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,9 +9,25 @@ class App extends React.Component {
     }
   }
 
+  onVersionSubmit (event) {
+    event.preventDefault();
+    console.log('submitted')
+  }
+
   render() {
     return (
-      <div>React is rendered</div>
+      <div>
+        <h1>Bible App</h1>
+        <form onSubmit={()=>{this.onVersionSubmit(event)}}>
+          <label>Choose A Translation</label><br></br>
+          <select name="version" id="bible-versions">
+            <option value="asv">American Standard Version</option>
+            <option value="kjv">King James Version</option>
+            <option value="geneva">Geneva</option>
+          </select><br></br>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     );
   }
 }
